c50b6695b2d2c14b401b32aba949fc683d7d260c
Kenny Oseleononmen <kenneth.o@pinecone.io>
2024-07-20 20:11:21 -0400

    🔥 [auth] register form loader

---
+++ src/components/register-form.jsx
@@ -0,0 +1,31 @@
+import { redirect } from "react-router-dom";
+import { supaClient } from "./supa-client";
+
+// Loader to prevent user from accessing the register page directly
+// when they have no business doing that
+export async function registerFormLoader() {
+  const {
+    data: { user },
+  } = await supaClient.auth.getUser();
+
+  // user doesn't exist, send them back home
+  if (!user) {
+    return redirect("/");
+  }
+
+  // user exists, check if they have a profile
+  const { data } = await supaClient
+    .from("user_profiles")
+    .select("*")
+    .eq("user_id", user?.id)
+    .single();
+
+  // user has a profile, send them back home
+  if (data?.username) {
+    return redirect("/");
+  }
+}
+
+export default function RegisterForm() {
+  return <div>RegisterForm</div>;
+}
