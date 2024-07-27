afcacbfa39dacaa3a539379740494ac25983aaa0
Kenny Oseleononmen <kenny1g@cs.stanford.edu>
2024-07-27 13:12:59 -0400

    prop validation + param for login-register-new

--- src/components/login-register-new.jsx
+++ src/components/login-register-new.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./login-register-new.css";
 
 import { useState } from "react";
@@ -6,11 +5,17 @@ import { useState } from "react";
 import Card from "./card";
 import LoginForm from "./login-form";
 import RegisterForm from "./register-form";
+import PropTypes from 'prop-types';
 
-export default function LoginReigsterNew() {
-  const [activeForm, setActiveForm] = useState("login");
+LoginRegisterNew.propTypes = {
+  _activeForm: PropTypes.string
+};
+
+export default function LoginRegisterNew({ _activeForm = "login" }) {
+  const [activeForm, setActiveForm] = useState(_activeForm);
   const [pageIndex, setPageIndex] = useState(0);
   const [completedSteps, setCompletedSteps] = useState(0);
+  console.log(activeForm)
 
   const handleTabClick = (form) => {
     setActiveForm(form);
@@ -45,6 +50,7 @@ export default function LoginReigsterNew() {
         onArrowClick={handleArrowClick}
         title=""
         pageIndex={pageIndex}
+        activeForm={activeForm}
       >
         {activeForm === "login" ? (
           <LoginForm index={pageIndex} onStep={handleStepComplete} />
