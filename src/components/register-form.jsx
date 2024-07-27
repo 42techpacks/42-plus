931faeff35b24ee8320aa572b4a71c2763a1d6d4
Kenny Oseleononmen <kenny1g@cs.stanford.edu>
2024-07-27 13:08:17 -0400

    move registerLoader to separate file

--- src/components/register-form.jsx
+++ src/components/register-form.jsx
@@ -6,30 +6,6 @@ import { supaClient } from "../supa-client";
 import Button from "./button";
 import Input from "./input";
 
-// Loader to prevent user from accessing the register page directly
-// when they have no business doing that
-export async function registerFormLoader() {
-  const {
-    data: { user },
-  } = await supaClient.auth.getUser();
-
-  // user doesn't exist, send them back home
-  if (!user) {
-    return redirect("/");
-  }
-
-  // user exists, check if they have a profile
-  const { data } = await supaClient
-    .from("user_profiles")
-    .select("*")
-    .eq("user_id", user?.id)
-    .single();
-
-  // user has a profile, send them back home
-  if (data?.username) {
-    return redirect("/");
-  }
-}
 
 export default function RegisterForm({ index, onStep }) {
   const navigate = useNavigate();
