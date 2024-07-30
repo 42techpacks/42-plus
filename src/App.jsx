9f2e6b76e6f2cfbcbdf52ff7a50fc2b29e53bce0
npcfaizan <faizan@fortytwoco.net>
2024-07-30 17:54:49 -0500

    revolutionized this app
    made it usable bc we can track it now.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import { createContext } from "react";
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import { useSession } from "./hooks/use-session";
+import posthog from "posthog-js";
 
 /* Component Imports */
 import Footer from "./components/footer";
 import Home from "./components/home";
 import TopBar from "./components/topbar";
+
+posthog.init(import.meta.env.VITE_POSTHOG_API_KEY, {
+  api_host: "https://us.i.posthog.com",
+  person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+});
 
 const router = createBrowserRouter([
   {
