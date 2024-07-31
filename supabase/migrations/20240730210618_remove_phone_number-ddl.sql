f0c4dba3b31f14bca8777c23ecb59911a9c74482
Kenny Oseleononmen <kenny1g@cs.stanford.edu>
2024-07-30 20:03:41 -0400

    ✏️ [supabase] fix seed.sql so we can set up local dbs

--- supabase/migrations/20240730210618_remove_phone_number-ddl.sql
+++ supabase/migrations/20240730210618_remove_phone_number-ddl.sql
@@ -1,7 +1,7 @@
+-- Remove phone number from user profiles since anon key can select all
+-- for the leaderboard
 alter table "public"."user_profiles" drop constraint "user_profiles_phone_number_key";
 
 drop index if exists "public"."user_profiles_phone_number_key";
 
 alter table "public"."user_profiles" drop column "phone_number";
-
-
